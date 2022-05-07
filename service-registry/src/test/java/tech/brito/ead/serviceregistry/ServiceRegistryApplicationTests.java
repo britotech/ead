17c153aa0269fdfb8fce52cd78e286ad36ca0a43
Anderson Brito Garcia <andersonbritogarcia@gmail.com>
2022-05-07 18:13:33 -0300

    Created service-registry for authuser and course services

---
+++ service-registry/src/test/java/tech/brito/ead/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.brito.ead.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
