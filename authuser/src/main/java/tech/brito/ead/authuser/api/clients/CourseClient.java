73e404c46a462172945d642a4e242c58c35f0263
Anderson B Garcia <andersonbritogarcia@gmail.com>
2022-08-13 16:59:03 -0300

    Implemented circuit break for synchronous access on the service authuser

--- authuser/src/main/java/tech/brito/ead/authuser/api/clients/CourseClient.java
+++ authuser/src/main/java/tech/brito/ead/authuser/api/clients/CourseClient.java
@@ -1,11 +1,10 @@
 package tech.brito.ead.authuser.api.clients;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +14,6 @@ import tech.brito.ead.authuser.api.models.CourseDTO;
 import tech.brito.ead.authuser.api.models.ResponsePageDTO;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.UUID;
 
 @Log4j2
@@ -31,7 +29,7 @@ public class CourseClient {
         this.restTemplate = restTemplate;
     }
 
-    @Retry(name = "retryIntance")
+    @CircuitBreaker(name = "circuitbreakerInstance")
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable) {
         execucoes = execucoes + 1;
         log.error("DataHora: {} execucoes -> {}", LocalDateTime.now(), execucoes);
